b4678a418a96ee948c04c14dda1ef8d41916d120
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-04-20 21:37:57 +0700

    Refactor integrate_by_trapezoids procedure

--- numerical-integration/trapezoidal_rule.py
+++ numerical-integration/trapezoidal_rule.py
@@ -1,17 +1,8 @@
-def integrate_by_trapezoids(points, fn, h):
-    def with_constant_step():
-        acc = 0
-        for i in range(1, len(points)):
-            acc += fn(points[i]) + fn(points[i - 1])
-        return (acc / 2) * h
+def integrate_by_trapezoids(fn, points):
+    acc = 0
+    for i in range(1, len(points)):
+        h = points[i] - points[i - 1]
 
-    def with_automatic_step():
-        acc = 0
-        for i in range(1, len(points)):
-            acc += (fn(points[i]) + fn(points[i - 1])) * h[i - 1]
-        return acc / 2
-
-    if isinstance(h, list):
-        return with_automatic_step()
-    else:
-        return with_constant_step()
+        acc += (fn(points[i]) + fn(points[i - 1])) * h
+        
+    return acc / 2
