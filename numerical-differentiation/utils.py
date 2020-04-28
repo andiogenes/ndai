ca77c3ebd86ccb811eb60ca92d499298e5ee7b49
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-04-28 20:56:51 +0700

    Add comments

--- numerical-differentiation/utils.py
+++ numerical-differentiation/utils.py
@@ -2,6 +2,10 @@ import parser
 
 
 def wrap_code(code, modules):
+    """
+    Compiles given line to Python bytecode and returns function which evaluates given bytecode with given modules in
+    environment.
+    """
     func_bytecode = parser.expr(code).compile()
 
     env = globals().copy()
