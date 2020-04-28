a2de75f98099fac5538a971aab3aeeb342afd151
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-04-28 16:22:12 +0700

    Add 'sensitivity' command

--- numerical-differentiation/main.py
+++ numerical-differentiation/main.py
@@ -1,4 +1,6 @@
 import argparse
+import random
+
 from derivatives import *
 
 import numpy as np
@@ -92,7 +94,80 @@ def process_function(_args):
 
 
 def process_sensitivity(_args):
-    pass
+    with open(_args.source, 'r') as stream:
+        try:
+            config = yaml.safe_load(stream)
+        except yaml.YAMLError as e:
+            print(e)
+            return
+
+    modules = ['math']
+
+    fun = wrap_code(config.get('fun', ''), modules)
+
+    derivatives = config.get('derivatives', {})
+    first_derivative = wrap_code(derivatives.get('first', ''), modules)
+    second_derivative = wrap_code(derivatives.get('second', ''), modules)
+    third_derivative = wrap_code(derivatives.get('third', ''), modules)
+
+    domain = config.get('domain', {})
+    inclusive = domain.get('inclusive', False)
+
+    a = wrap_code(str(domain.get('a', 0)), modules)(0)
+    b = wrap_code(str(domain.get('b', 0)), modules)(0)
+
+    step = wrap_code(str(config.get('step', 0)), modules)(0)
+    if inclusive:
+        b += step
+
+    perturbation = config.get('perturbation', {})
+    delta = wrap_code(str(perturbation.get('delta', 0)), modules)(0)
+
+    arg_table = list(np.arange(a, b, step))
+    val_table = list(map(lambda x: fun(x) + random.uniform(-delta, delta), arg_table))
+
+    diff_1 = FirstDerivative(val_table, step)
+    diff_2 = SecondDerivative(val_table, step)
+    diff_3 = ThirdDerivative(val_table, step)
+
+    d_1_real = [first_derivative(x) for x in arg_table]
+    d_2_real = [second_derivative(x) for x in arg_table]
+    d_3_real = [third_derivative(x) for x in arg_table]
+
+    d_1_numerical = [diff_1(i) for i in range(0, len(arg_table))]
+    d_2_numerical = [diff_2(i) for i in range(0, len(arg_table))]
+    d_3_numerical = [diff_3(i) for i in range(0, len(arg_table))]
+
+    table = PrettyTable([
+        'x',
+        'f(x)',
+        'f\'(x)',
+        'f\'(x) (numerical)',
+        'Δ\'',
+        'f\'\'(x)',
+        'f\'\'(x) (numerical)',
+        'Δ\'\'',
+        'f\'\'\'(x)',
+        'f\'\'\'(x) (numerical)',
+        'Δ\'\'\''
+    ])
+
+    for i in range(0, len(arg_table)):
+        table.add_row([
+            arg_table[i],
+            val_table[i],
+            d_1_real[i],
+            d_1_numerical[i],
+            abs(d_1_real[i] - d_1_numerical[i]),
+            d_2_real[i],
+            d_2_numerical[i],
+            abs(d_2_real[i] - d_2_numerical[i]),
+            d_3_real[i],
+            d_3_numerical[i],
+            abs(d_3_real[i] - d_3_numerical[i])
+        ])
+
+    print(table)
 
 
 def parse_command_line():
